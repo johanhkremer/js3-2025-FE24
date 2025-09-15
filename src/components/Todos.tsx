999a9540fdbd70a43a1894c07bcbaf39735424b1
Johan Kremer <johan.h.kremer@gmail.com>
2025-09-15 11:56:01 +0200

    add component with fetch todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,22 +1,46 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-// interface JsonDataTodo {
-//     userId: number,
-//     id: number,
-//     title: string,
-//     completed: boolean
-// }
+interface JsonDataTodo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
 
 const Todos = () => {
+    const [todos, setTodos] = useState<JsonDataTodo[] | undefined>(undefined)
+    const [refetch, setRefetch] = useState<boolean>(false)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
-            .then(json => console.log(json))
-    }, [])
+            .then((json: JsonDataTodo[]) => {
+                console.log(json)
+                setTodos(json)
+            })
+    }, [refetch])
+
+    const handelRefetch = () => {
+        setRefetch(!refetch)
+    }
 
     return (
-        <div>Todos</div>
+        <>
+            <div>Todos</div>
+            <button onClick={handelRefetch}>Refetch data</button>
+            <ul>
+                {todos && todos.map((todo) => (
+                    <li key={todo.id}>
+                        <h2 >{todo.title}</h2>
+                        {todo.completed
+                            ? <p>Todo Completed!</p>
+                            : <p>Todo not completed!</p>
+                        }
+                    </li>
+                ))
+                }
+            </ul>
+        </>
     )
 }
 
