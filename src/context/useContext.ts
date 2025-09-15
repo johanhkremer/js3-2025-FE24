571bec11333eca7214fa87a420aba36daff2bb5a
Johan Kremer <johan.h.kremer@gmail.com>
2025-09-15 12:17:25 +0200

    remove user card components, types, context and styles

--- src/context/useContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import type { User } from "../types/user";
-
-export const UserCardContext = createContext<User | undefined>(undefined)
--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: string,
-    name: string,
-    email: string,
-    role: string,
-    avatarUrl: string,
-    isOnline: boolean
-}
